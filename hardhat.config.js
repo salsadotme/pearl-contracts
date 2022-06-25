d333e4b9daa1a9f6dc7fa8e595a7d76b105dc6d0
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2022-06-25 07:52:01 -0400

    adding hardhat configuration for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +18,43 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      // Disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      /*
+        notice no   mnemonic here? it will just use account 0 of the hardhat node to deploy
+        (you can put in a mnemonic here to set the deployer locally)
+      */
+    },
+    celo: {
+      url: process.env.CELO_RPC,
+      accounts: [ process.env.ADMIN_PRIVATE_KEY ],
+      gas: 2100000, gasPrice: 8000000000
+    },
+    mumbai: {
+      url: process.env.MUMBAI_RPC,
+      accounts: [ process.env.ADMIN_PRIVATE_KEY ],
+      gas: 2100000, gasPrice: 8000000000
+    },
+    mumbai: {
+      url: process.env.RINKEBY_RPC,
+      accounts: [ process.env.ADMIN_PRIVATE_KEY ],
+      gas: 2100000, gasPrice: 8000000000
+    },
+  }
 };
